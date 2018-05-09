481fa1d1d549127a49b78c733fb263904db9a064
ralphhm <ralphhm+github@gmail.com>
2018-05-09 02:22:04 +0200

    Fix unit tests - Provide default values to Review model - Enable mocking of final methods

--- app/src/main/java/de/rhm/reviews/api/model/Review.kt
+++ app/src/main/java/de/rhm/reviews/api/model/Review.kt
@@ -2,8 +2,8 @@ package de.rhm.reviews.api.model
 
 import com.google.gson.annotations.SerializedName
 
-class Review(@SerializedName("reviewerName") val author: String,
-             @SerializedName("title") val title: String,
-             @SerializedName("message") val message: String,
-             @SerializedName("date") val date: String,
-             @SerializedName("rating") val rating: Float)
+class Review(@SerializedName("reviewerName") val author: String = "",
+             @SerializedName("title") val title: String = "",
+             @SerializedName("message") val message: String = "",
+             @SerializedName("date") val date: String = "",
+             @SerializedName("rating") val rating: Float = 0f)
